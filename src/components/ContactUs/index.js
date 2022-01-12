e0eb0ebcd75c4cadae46dca000987a863030d5e7
Maheshvar Pandey <mkp10697@gmail.com>
2022-01-12 23:13:54 +0530

    Added the soial media links

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -146,7 +146,11 @@ function ContactUs(props) {
 								<div className="container max-w-screen-lg mx-auto">
 									<div className="lg:pt-36">
 										<div className="flex flex-wrap justify-center gap-2">
-											<a href="https://facebook.com/maheshwar.pandey.58" target="_blank" className="bg-blue-500 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded" rel="noreferrer">
+											<a
+												href="https://facebook.com/maheshwar.pandey.58"
+												target="_blank"
+												className="bg-blue-500 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded"
+												rel="noreferrer">
 												<svg
 													className="w-12 h-12 fill-current cursor-pointer"
 													role="img"
@@ -155,7 +159,11 @@ function ContactUs(props) {
 													<path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
 												</svg>
 											</a>
-											<a href="https://twitter.com/KumarMaheshvar" target="_blank" className="bg-blue-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded" rel="noreferrer">
+											<a
+												href="https://twitter.com/KumarMaheshvar"
+												target="_blank"
+												className="bg-blue-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded"
+												rel="noreferrer">
 												<svg
 													className="w-12 h-12 fill-current cursor-pointer"
 													role="img"
@@ -182,7 +190,11 @@ function ContactUs(props) {
 													<path d="M12.017 0C5.396 0 .029 5.367.029 11.987c0 5.079 3.158 9.417 7.618 11.162-.105-.949-.199-2.403.041-3.439.219-.937 1.406-5.957 1.406-5.957s-.359-.72-.359-1.781c0-1.663.967-2.911 2.168-2.911 1.024 0 1.518.769 1.518 1.688 0 1.029-.653 2.567-.992 3.992-.285 1.193.6 2.165 1.775 2.165 2.128 0 3.768-2.245 3.768-5.487 0-2.861-2.063-4.869-5.008-4.869-3.41 0-5.409 2.562-5.409 5.199 0 1.033.394 2.143.889 2.741.099.12.112.225.085.345-.09.375-.293 1.199-.334 1.363-.053.225-.172.271-.401.165-1.495-.69-2.433-2.878-2.433-4.646 0-3.776 2.748-7.252 7.92-7.252 4.158 0 7.392 2.967 7.392 6.923 0 4.135-2.607 7.462-6.233 7.462-1.214 0-2.354-.629-2.758-1.379l-.749 2.848c-.269 1.045-1.004 2.352-1.498 3.146 1.123.345 2.306.535 3.55.535 6.607 0 11.985-5.365 11.985-11.987C23.97 5.39 18.592.026 11.985.026L12.017 0z" />
 												</svg>
 											</a> */}
-											<a href="https://www.linkedin.com/in/maheshvar-pandey-257230149/" target="_blank" className="bg-blue-600 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded" rel="noreferrer">
+											<a
+												href="https://www.linkedin.com/in/maheshvar-pandey-257230149/"
+												target="_blank"
+												className="bg-blue-600 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded"
+												rel="noreferrer">
 												<svg
 													className="w-12 h-12 fill-current cursor-pointer"
 													role="img"
@@ -193,6 +205,41 @@ function ContactUs(props) {
 													</g>
 												</svg>
 											</a>
+											<a
+												href="https://www.instagram.com/mr_panday_jee"
+												target="_blank"
+												className="bg-blue-600 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded"
+												rel="noreferrer">
+												<svg
+													className="instagram-logo"
+													id="Layer_1"
+													xmlns="http://www.w3.org/2000/svg"
+													width={100}
+													height={100}
+													viewBox="0 0 551.034 551.034"
+													style={{
+														enableBackground: "new 0 0 551.034 551.034",
+													}}
+													xmlSpace="preserve">
+													<path
+														className="logo"
+														id="XMLID_17_"
+														d="M386.878,0H164.156C73.64,0,0,73.64,0,164.156v222.722 c0,90.516,73.64,164.156,164.156,164.156h222.722c90.516,0,164.156-73.64,164.156-164.156V164.156 C551.033,73.64,477.393,0,386.878,0z M495.6,386.878c0,60.045-48.677,108.722-108.722,108.722H164.156 c-60.045,0-108.722-48.677-108.722-108.722V164.156c0-60.046,48.677-108.722,108.722-108.722h222.722 c60.045,0,108.722,48.676,108.722,108.722L495.6,386.878L495.6,386.878z"
+													/>
+													<path
+														id="XMLID_81_"
+														fill="#555"
+														d="M275.517,133C196.933,133,133,196.933,133,275.516 s63.933,142.517,142.517,142.517S418.034,354.1,418.034,275.516S354.101,133,275.517,133z M275.517,362.6 c-48.095,0-87.083-38.988-87.083-87.083s38.989-87.083,87.083-87.083c48.095,0,87.083,38.988,87.083,87.083 C362.6,323.611,323.611,362.6,275.517,362.6z"
+													/>
+													<circle
+														id="XMLID_83_"
+														fill="#555"
+														cx="418.306"
+														cy="134.072"
+														r="34.149"
+													/>
+												</svg>
+											</a>
 											{/* <a className="bg-red-600 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
 												<svg
 													className="w-12 h-12 fill-current cursor-pointer"
@@ -201,7 +248,11 @@ function ContactUs(props) {
 													<path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z" />
 												</svg>
 											</a> */}
-											<a href="https://github.com/maheshvarpandey" target="_blank" className="bg-gray-700 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded" rel="noreferrer">
+											<a
+												href="https://github.com/maheshvarpandey"
+												target="_blank"
+												className="bg-gray-700 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded"
+												rel="noreferrer">
 												<svg
 													xmlns="http://www.w3.org/2000/svg"
 													xmlnsXlink="http://www.w3.org/1999/xlink"
